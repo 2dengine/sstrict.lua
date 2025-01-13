93db5c41334dd933f046f1727aec1157b879421a
2dengine <66012682+2dengine@users.noreply.github.com>
2025-01-13 14:33:43 +0200

    Update scan.lua

--- .github/workflows/scan.lua
+++ .github/workflows/scan.lua
@@ -2,20 +2,21 @@ lfs = require('lfs')
 require('sstrict')
 
 function scan(path)
-  print('scanning:'..path)
   for file in lfs.dir(path) do
     if file ~= '.' and file ~= '..' then
       local full = path..'/'..file
-      print(full)
       local attr = lfs.attributes(full)
       if attr.mode == 'directory' then
         scan(full)
       else
         if file:match('%.lua$') then
+          print(full)
           dofile(full)
         end
       end
     end
   end
 end
+print('scanning...')
 scan('.')
+print('all done')
